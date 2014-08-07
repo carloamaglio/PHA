8dde1db070723a0769571e723574069f2f2ccdb3
Carlo <amaglio.c@gmail.com>
2014-08-07 21:46:14 +0200

    swing

--- jhome/src/app/App.java
+++ jhome/src/app/App.java
@@ -1,11 +1,34 @@
 package app;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
 public class App {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+	}
+
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("Personal Home Automation");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add the ubiquitous "Hello World" label.
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
